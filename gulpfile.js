f2416721b77bff37d4de8fccc81d024848f21b70
Jose Murillo <jp1995azul@hotmail.com>
2021-03-29 14:31:47 -0600

    Adding browser-sync package for development, updating gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,67 @@
 
+const config = require("./src/config.json");
+
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const htmlmin = require('gulp-htmlmin');
 const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 
 
 
 sass.compiler = require('node-sass');
 
-//html
+//html TODO
 gulp.task('minify-html', () => {
   return gulp.src('./src/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest(config.html.build));
 });
 
-// Stylesheets
+// Sass processing
 gulp.task('sass', function () {
-    return gulp.src('./src/scss/**/*.scss')
+    return gulp.src(config.sass.src)
       .pipe(sass.sync().on('error', sass.logError))
-      .pipe(gulp.dest('./css'));
+      .pipe(autoprefixer({
+        overrideBrowsersList: ['last 80 versions'],
+      }))
+      .pipe(gulp.dest(config.sass.dest))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
   });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+// CSS minify
+  gulp.task('minify-css', function() {
+    return gulp.src(config.css.src)
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(gulp.dest(config.css.dest));
+  });
+
+//Watch for file changes and reload
+gulp.task('watch', function() {
+  gulp.watch(config.sass.src, gulp.series('sass'));
+  gulp.watch(config.html.src, gulp.series(browserSync.reload));
 });
 
-// Add vendor prefix
-gulp.task('prefix', function(){
-  return gulp.src('./css/*.css')
-  .pipe(autoprefixer({
-    overrideBrowsersList: ['last 80 version'],
-    cascade: false
-  }))
-  .pipe(gulp.dest('./css'));
-  
+//Browser reload
+gulp.task('browserSync', function(callback) {
+  browserSync.init({
+      server: {
+        baseDir: config.baseDir
+      }
+  }, callback)
 });
 
-
-
-gulp.task('minify-css', () => {
-  return gulp.src('./css/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./css'));
-});
+//default task for dev
+gulp.task('default', gulp.series('sass', 'browserSync', 'watch'));
 
 
 
-gulp.task('build', gulp.series('sass', 'prefix', 'minify-css', 'minify-html'));
+
+
+
+gulp.task('build', gulp.series('sass', 'minify-css', 'minify-html'));
 
 
